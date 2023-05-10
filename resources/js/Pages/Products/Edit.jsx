47a5fbb6fb771f3ae8505c7f15d125acb81c9089
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-10 04:44:14 -0500

    feat(product): select multiple categories

--- resources/js/Pages/Products/Edit.jsx
+++ resources/js/Pages/Products/Edit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import {
     Box,
@@ -14,12 +14,14 @@ import {
     NumberInput,
     NumberInputField,
     Flex,
+    Textarea,
 } from "@chakra-ui/react";
 import { Inertia } from "@inertiajs/inertia";
 import { InertiaLink } from "@inertiajs/inertia-react";
 import Layout from "../../components/Layout";
+import Select from "react-select";
 
-const Edit = ({ product, auth }) => {
+const Edit = ({ product, auth, categories }) => {
     const { register, handleSubmit, setValue } = useForm({
         defaultValues: {
             name: product.name,
@@ -28,8 +30,35 @@ const Edit = ({ product, auth }) => {
         },
     });
 
+    const [selectedCategories, setSelectedCategories] = useState(
+        product.categories.map((category) => ({
+            value: category.id,
+            label: category.name,
+        }))
+    );
+
+    const categoryOptions = categories.map((category) => ({
+        value: category.id,
+        label: category.name,
+    }));
+
+    useEffect(() => {
+        const selectedOptions = product.categories.map((category) => ({
+            value: category.id,
+            label: category.name,
+        }));
+        setSelectedCategories(selectedOptions);
+    }, [product.categories]);
+
+    const handleCategoriesChange = (selectedOptions) => {
+        setSelectedCategories(selectedOptions);
+    };
+
     const onSubmit = (data) => {
-        Inertia.put(`/products/${product.id}`, data);
+        Inertia.put(`/products/${product.id}`, {
+            ...data,
+            categories: selectedCategories.map((option) => option.value),
+        });
     };
 
     return (
@@ -44,7 +73,6 @@ const Edit = ({ product, auth }) => {
                             <FormLabel htmlFor="name">Name</FormLabel>
                             <Input
                                 id="name"
-                                placeholder="Enter product name"
                                 {...register("name", { required: true })}
                             />
                         </FormControl>
@@ -52,27 +80,34 @@ const Edit = ({ product, auth }) => {
                             <FormLabel htmlFor="description">
                                 Description
                             </FormLabel>
-                            <Input
+                            <Textarea
                                 id="description"
-                                placeholder="Enter product description"
-                                {...register("description")}
+                                {...register("description", { required: true })}
                             />
                         </FormControl>
                         <FormControl id="price" mb={5}>
                             <FormLabel>Price</FormLabel>
-                            <NumberInput>
+                            <NumberInput
+                                min={0}
+                                precision={2}
+                                format={(value) => `${value}`}
+                            >
                                 <NumberInputField
                                     {...register("price", {
                                         required: true,
                                         min: 0,
                                     })}
-                                    placeholder="Enter product price"
                                 />
-                                <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                </NumberInputStepper>
                             </NumberInput>
+                        </FormControl>
+                        <FormControl id="categories" mb={5}>
+                            <FormLabel>Categories</FormLabel>
+                            <Select
+                                options={categoryOptions}
+                                isMulti
+                                value={selectedCategories}
+                                onChange={handleCategoriesChange}
+                            />
                         </FormControl>
                         <Flex justifyContent="flex-end">
                             <InertiaLink href="/products">
