01a56bd9da47457d4e038ed49164db86495f68c1
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-09 23:23:21 -0500

    feat(user): remove customer table and model

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\OrderController;
 
 /*
