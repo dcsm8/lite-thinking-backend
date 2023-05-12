a0ae7c008d53d4a19e10bc94650db03ece1a61dd
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-12 14:52:07 -0500

    build 2

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
-        react(),
+        react(), // React plugin that we installed for vite.js
         laravel({
             input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
     ],
-    build: {
-        minify: false,
-    },
 });
