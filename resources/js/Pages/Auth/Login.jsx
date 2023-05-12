f1aae3fa2f0a266349e1eb45c2c71d24ffb5e060
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-12 15:35:46 -0500

    styles: login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Inertia } from "@inertiajs/inertia"; // Import Inertia
+import { Inertia } from "@inertiajs/inertia";
 import { useForm } from "react-hook-form";
 import {
     Box,
     Button,
+    Flex,
     FormControl,
     FormErrorMessage,
     FormLabel,
     Input,
     Stack,
 } from "@chakra-ui/react";
-import { InertiaLink, usePage } from "@inertiajs/inertia-react"; // Import InertiaLink and usePage
+import { InertiaLink, usePage } from "@inertiajs/inertia-react";
 
 const Login = () => {
     const { errors } = usePage().props;
@@ -21,49 +22,53 @@ const Login = () => {
     };
 
     return (
-        <Box
-            p={8}
-            borderWidth={1}
-            borderRadius={8}
-            width={{ base: "90%", md: "40%" }}
-            margin="auto"
-        >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Stack spacing={4}>
-                    <FormControl isInvalid={errors.email}>
-                        <FormLabel htmlFor="email">Email address</FormLabel>
-                        <Input
-                            type="email"
-                            id="email"
-                            {...register("email", { required: true })}
-                        />
-                        <FormErrorMessage>{errors.email}</FormErrorMessage>
-                    </FormControl>
+        <Flex minHeight="100vh" alignItems="center" justifyContent="center">
+            <Box
+                p={8}
+                borderWidth={1}
+                borderRadius={8}
+                width={{ base: "90%", md: "40%" }}
+                boxShadow="xl"
+            >
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={4}>
+                        <FormControl isInvalid={errors.email}>
+                            <FormLabel htmlFor="email">Email address</FormLabel>
+                            <Input
+                                type="email"
+                                id="email"
+                                {...register("email", { required: true })}
+                            />
+                            <FormErrorMessage>{errors.email}</FormErrorMessage>
+                        </FormControl>
 
-                    <FormControl isInvalid={errors.password}>
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <Input
-                            type="password"
-                            id="password"
-                            {...register("password", { required: true })}
-                        />
-                        <FormErrorMessage>{errors.password}</FormErrorMessage>
-                    </FormControl>
+                        <FormControl isInvalid={errors.password}>
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                            <Input
+                                type="password"
+                                id="password"
+                                {...register("password", { required: true })}
+                            />
+                            <FormErrorMessage>
+                                {errors.password}
+                            </FormErrorMessage>
+                        </FormControl>
 
-                    <Button
-                        colorScheme="teal"
-                        isLoading={formState.isSubmitting}
-                        type="submit"
-                    >
-                        Login
-                    </Button>
+                        <Button
+                            colorScheme="teal"
+                            isLoading={formState.isSubmitting}
+                            type="submit"
+                        >
+                            Login
+                        </Button>
 
-                    <InertiaLink href="/register" fontWeight="bold">
-                        Create an account
-                    </InertiaLink>
-                </Stack>
-            </form>
-        </Box>
+                        <InertiaLink href="/register" fontWeight="bold">
+                            Create an account
+                        </InertiaLink>
+                    </Stack>
+                </form>
+            </Box>
+        </Flex>
     );
 };
 
