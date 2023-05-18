d8c0fd262e7b7df0ed0d905330d81248a741b998
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-18 13:48:45 -0500

    build 5

---
+++ public/build/assets/chunk-MUMUYFOI-f426a674.js
@@ -0,0 +1,1 @@
+import{S as t}from"./chunk-O5CRURSQ-6dee9f37.js";import{f as o,j as s}from"./app-e676fbb6.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
